d2f5eb12997571401378f5f0ada8219ff0fe7b3d
Mappy The Kat <mappythekat@naver.com>
2016-04-12 03:59:20 +0900

    이것저것 찔러보는중 / 그와중에 Nuget패키지 추가

---
+++ SE_Project/SE_Project/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SE_Project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); //<- 대체 왜??
+        }
+
+        string MainText;
+
+        public void OnSomeButtonClicked(object sender, EventArgs e)
+        {
+            MainText = "FORK YOU";
+               
+        }
+    }
+}
