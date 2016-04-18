3134c4291e9bb08893d6a4a0063906619823ca05
wrrho <hahi3347@gmail.com>
2016-04-18 22:43:04 +0900

    페이지 나눔

---
+++ SE_Project/SE_Project/AppCommon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Project
+{
+    public static class AppCommon
+    {
+    }
+}
---
+++ SE_Project/SE_Project/Scene/MainPage/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace SE_Project.Scene.MainPage
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
